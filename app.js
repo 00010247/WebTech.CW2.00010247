4228e69395b5a3fa7a122b5b5a12a3820801c4cf
00010247 <73783958+00010247@users.noreply.github.com>
2021-04-07 16:34:49 +0500

    create pug file was added and styled

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ app.set('view engine', 'pug')
 app.use('/static', express.static('public'))
 
 //localhost:8000
-app.get('/', (req, res) => { 
+app.get('/', (req, res) => {
 res.render('home')
 })
-
+app.get('/create', (req, res) => {
+    res.render('create')
+})
 app.listen(8000, err => {
     if (err) console.log(err)
 
